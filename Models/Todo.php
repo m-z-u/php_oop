58abf7ed81fe08207cdab9cceca40139c59f6071
Atsushi <atsushi88885@gmail.com>
2019-08-01 11:49:43 +0800

    新規作成機能完成

--- Models/Todo.php
+++ Models/Todo.php
@@ -1,3 +1,33 @@
 <?php
 
 require_once('config/dbconnect.php');
+
+// Todo
+class Todo
+{
+    // プロパティ
+    private $table = 'tasks';
+    private $db_manager;
+
+    // インスタンス化した時に呼ばれるメソッド
+    public function __construct()
+    {
+        // db_managerプロパティは、
+        // DbManagerクラスのインスタンス
+        $this->db_manager = new DbManager();
+        $this->db_manager->connect();
+    }
+
+    public function create($name)
+    {
+        // DBに保存
+        // このクラスのインスタンスの
+        // db_managerプロパティの
+        // DbManagerクラスのインスタンス
+        // dbhプロパティの
+        // PDOのインスタンス
+        // prepareメソッドを実行
+        $stmt = $this->db_manager->dbh->prepare('INSERT INTO ' . $this->table . ' (name) VALUES (?)');
+        $stmt->execute([$name]);
+    }
+}
--- create.php
+++ create.php
@@ -1,10 +1,22 @@
 <?php
 
-// ADDボタンを押したら、
-// ユーザーが入力した内容をDBに保存して、
+// Todo.phpを読み込む
+require_once('Models/Todo.php');
 
+// ユーザーが入力した内容を取得
+// $taskっていう変数にユーザーが入力した内容代入
+// $task = ユーザーが入力した内容
+$task = $_POST['task'];
 
-//
+// DBに保存
+
+// Todoクラスをインスタンス化して$todoに代入
+$todo = new Todo();
+
+// Todoクラスのインスタンスの
+// createメソッドを実行
+$todo->create($task);
+
 
 // 一覧画面に戻る
-header('location: index.php');
+header('Location: index.php');
